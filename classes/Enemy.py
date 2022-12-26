4c27ecbfeef0b51c716635946904484ae485c6da
Andre Alves <andrealvesc09@gmail.com>
2022-12-25 23:38:04 -0300

    jogo pronto

---
+++ classes/Enemy.py
@@ -0,0 +1,18 @@
+import pygame
+class Enemy(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load('img/blob.png')
+		self.image = pygame.transform.scale(self.image, (18, 18))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.move_direction = 1
+		self.move_counter = 0
+
+	def update(self):
+		self.rect.x += self.move_direction
+		self.move_counter += 0.5
+		if abs(self.move_counter) > 25:
+			self.move_direction *= -1
+			self.move_counter *= -1
---
+++ classes/variaveis_globais.py
@@ -0,0 +1,8 @@
+import pygame
+
+class variaveis():
+
+    screen_width = 500
+    screen_height = 500
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    tile_size = 25
